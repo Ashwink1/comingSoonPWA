5a3182d6d6abbc27f79e9be5356a96b4f3946be9
Ashwin krishna <ashwin@fptechscience.com>
2017-11-10 23:23:41 +0530

    Changes in the config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import _ from 'lodash';
-import printMe from './print.js';
-
+import './style.css';
+import Icon from './icon.jpg';
 function component() {
-    var element = document.createElement('div');
-      var btn = document.createElement('button');
+  var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = Icon;
 
-      btn.innerHTML = 'Click me and check the console!';
-      btn.onclick = printMe;
+  element.appendChild(myIcon);
 
-          element.appendChild(btn);
 
-    return element;
+  return element;
 }
-
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');//Html creation plugin
+const CleanWebpackPlugin = require('clean-webpack-plugin');//clean up dist
 
 module.exports = {
-    entry:{
+    entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        print: './src/print.js',
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            filename: 'index.html'
+        }),
+    ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
-     module: {
-       rules: [
-             {
-               test: /\.css$/,
-               use: [
-                 'style-loader',
-                 'css-loader'
-               ]
-             },
-             {
-               test: /\.(png|svg|jpg|gif)$/,
-               use: [
-                 'file-loader'
-               ]
-             }
-       ]
-     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
 };
